2e25aef4112d645f02f985a0861a739940f0cfb6
ttaniguchi <ttaniguchi@oikaze.jp>
2019-06-23 08:29:16 +0900

    Update MainTabNavigator

--- src/navigation/MainTabNavigator.js
+++ src/navigation/MainTabNavigator.js
@@ -1,63 +1,87 @@
-import React from 'react';
-import { Platform } from 'react-native';
+import { Constants } from 'expo';
+import { createStackNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-native-tabs';
+
+import HomeScreen from 'app/src/screens/HomeScreen';
+import SettingsScreen from 'app/src/screens/SearchScreen';
+import NotificationScreen from  'app/src/screens/NotificationScreen';
+
+import UserScreen from 'app/src/screens/UserScreen';
 import {
-  createStackNavigator,
-  createBottomTabNavigator,
-} from 'react-navigation';
+  HomeTabIcon,
+  SearchTabIcon,
+  TakeTabIcon,
+  NotificationScreen,
+  MeTabIcon,
+  TabBar
+} from 'app/src/components/Tab';
 
-import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
-
-const HomeStack = createStackNavigator({
-  Home: HomeScreen,
+//stackNavigatorを簡単に作れるようにするための関数
+const createTabStack = (title,screen) => createStackNavigator({
+  [title]: {screen},
 });
 
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
-};
 
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
-});
+// import React from 'react';
+// import { Platform } from 'react-native';
+// import {
+//   createStackNavigator,
+//   createBottomTabNavigator,
+// } from 'react-navigation';
 
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
-    />
-  ),
-};
+// import TabBarIcon from '../components/TabBarIcon';
+// import HomeScreen from '../screens/HomeScreen';
+// import LinksScreen from '../screens/LinksScreen';
+// import SettingsScreen from '../screens/SettingsScreen';
 
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
-});
+// const HomeStack = createStackNavigator({
+//   Home: HomeScreen,
+// });
 
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
-    />
-  ),
-};
+// HomeStack.navigationOptions = {
+//   tabBarLabel: 'Home',
+//   tabBarIcon: ({ focused }) => (
+//     <TabBarIcon
+//       focused={focused}
+//       name={
+//         Platform.OS === 'ios'
+//           ? `ios-information-circle${focused ? '' : '-outline'}`
+//           : 'md-information-circle'
+//       }
+//     />
+//   ),
+// };
 
-export default createBottomTabNavigator({
-  HomeStack,
-  LinksStack,
-  SettingsStack,
-});
+// const LinksStack = createStackNavigator({
+//   Links: LinksScreen,
+// });
+
+// LinksStack.navigationOptions = {
+//   tabBarLabel: 'Links',
+//   tabBarIcon: ({ focused }) => (
+//     <TabBarIcon
+//       focused={focused}
+//       name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+//     />
+//   ),
+// };
+
+// const SettingsStack = createStackNavigator({
+//   Settings: SettingsScreen,
+// });
+
+// SettingsStack.navigationOptions = {
+//   tabBarLabel: 'Settings',
+//   tabBarIcon: ({ focused }) => (
+//     <TabBarIcon
+//       focused={focused}
+//       name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+//     />
+//   ),
+// };
+
+// export default createBottomTabNavigator({
+//   HomeStack,
+//   LinksStack,
+//   SettingsStack,
+// });
